b807e4e625c2ca30a78274c9ca57b5922491b22c
PaulinaVPG <pvpg@live.com.mx>
2020-12-27 17:08:41 -0600

    packages and gatsby config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,27 +5,19 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-prefetch-google-fonts',
-      options: {
-        fonts: [
-          {
-            family: 'Lato',
-            variants: ['400', '400i', '700', '700i', '900'],
-          },
-          {
-            family: 'Amiri',
-            variants: ['400', '400i', '700', '700i'],
-          },
-        ],
-      },
-    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
         icon: 'src/images/favicon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
       },
     },
   ],
