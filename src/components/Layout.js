472597a0dee22d00dfd2b34b15a10506a0424953
PaulinaVPG <pvpg@live.com.mx>
2020-12-27 19:36:48 -0600

    props from fragment

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
-const Layout = ({ isHomepage, children }) => (
+const Layout = ({ isHomepage, children, navigation }) => (
   <>
-    <Header isHomepage={isHomepage} />
+    <Header isHomepage={isHomepage} navigation={navigation} />
     {children}
     <Footer />
   </>
