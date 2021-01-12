bbc2c060b4f5e42afc9d8889a3797306e254f71b
PaulinaVPG <pvpg@live.com.mx>
2021-01-12 13:54:25 -0600

    remove old styles import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
-import './src/styles/reset.css'
-import './src/styles/common.css'
-import './src/styles/style.css'
-
 import { registerLinkResolver } from 'gatsby-source-prismic-graphql'
 import linkResolver from './src/utils/linkResolver'
 
