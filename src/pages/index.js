0e7e8fd41a8155768f6ef665e010fe77e1d863b6
PaulinaVPG <pvpg@live.com.mx>
2021-01-13 21:31:15 -0600

    Update branch 3-add-slicezone

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,83 +51,85 @@ export const query = graphql`
               thumbnails
               alt
             }
-          }
-<<<<<<< HEAD
-          banner_link_label
-          banner_background
-          body {
-            __typename
-            ...on PRISMIC_HomepageBodyText {
-              type
-              primary {
-                columns
-                content
-              }
-            }
-            ...on PRISMIC_HomepageBodyQuote {
-              type
-              primary {
-                quote
-              }
-            }
-            ...on PRISMIC_HomepageBodyFull_width_image {
-              type
-              primary {
-                full_width_image
-                
-              }
-            }
-            ...on PRISMIC_HomepageBodyImage_gallery {
-              type
-              primary {
-                gallery_title
-              }
-              fields {
-                image
-                image_description
-                link {
-                  _linkType
-                  ...on PRISMIC_Page {
-                    _meta {
-                      type
-                      uid
-                    }
-                  }
-                  ...on PRISMIC_Homepage {
-                    _meta {
-                      type
-                    }
+            body {
+              ... on PrismicHomepageBodyText {
+                slice_type
+                primary {
+                  columns
+                  content {
+                    raw
                   }
                 }
-                link_label
               }
-            }
-            ...on PRISMIC_HomepageBodyImage_highlight {
-              type
-              primary {
-                featured_image
-                title
-                description
-                link {
-                  _linkType
-                  ...on PRISMIC_Page {
-                    _meta {
-                      type
-                      uid
-                    }
-                  }
-                  ...on PRISMIC_Homepage {
-                    _meta {
-                      type
-                    }
+              ... on PrismicHomepageBodyQuote {
+                slice_type
+                primary {
+                  quote {
+                    raw
                   }
                 }
-                link_label
+              }
+              ... on PrismicHomepageBodyFullWidthImage {
+                slice_type
+                primary {
+                  full_width_image {
+                    url
+                    thumbnails
+                  }
+                }
+              }
+              ... on PrismicHomepageBodyImageGallery {
+                slice_type
+                primary {
+                  gallery_title {
+                    raw
+                  }
+                }
+                items {
+                  image {
+                    url
+                    thumbnails
+                    alt
+                  }
+                  image_description {
+                    raw
+                  }
+                  link {
+                    url
+                    type
+                    uid
+                  }
+                  link_label {
+                    raw
+                  }
+                }
+              }
+              ... on PrismicHomepageBodyImageHighlight {
+                slice_type
+                primary {
+                  featured_image {
+                    url
+                    thumbnails
+                    alt
+                  }
+                  title {
+                    raw
+                  }
+                  description {
+                    raw
+                  }
+                  link {
+                    url
+                    type
+                    uid
+                  }
+                  link_label {
+                    raw
+                  }
+                }
               }
             }
           }
-=======
->>>>>>> db3d8663697b5f433b8a5ba8626d1cc3d284e5d3
         }
       }
     }
