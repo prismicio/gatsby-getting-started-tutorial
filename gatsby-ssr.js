1cdfc73a96c4c4e67adc110ed8e85f4fb3b0c3f6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-05-13 10:54:15 -1000

    refactor: move CSS imports to Gatsby APIs

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,10 @@ import { PrismicPreviewProvider } from 'gatsby-plugin-prismic-previews'
 
 import 'gatsby-plugin-prismic-previews/dist/styles.css'
 
+import './src/styles/reset.css'
+import './src/styles/common.css'
+import './src/styles/style.css'
+
 export const wrapRootElement = ({ element }) => (
   <PrismicPreviewProvider>{element}</PrismicPreviewProvider>
 )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import './../styles/reset.css'
-import './../styles/common.css'
-import './../styles/style.css'
 
 const Layout = ({ isHomepage, children, navigation }) => (
   <>
