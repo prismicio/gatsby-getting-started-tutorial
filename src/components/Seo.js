dbd87a91aa1bcb3265ea31cd6073fefc548a78d4
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2021-05-13 10:07:29 -1000

    fix: GraphQL query issue due to filename casing

---
+++ src/components/Seo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+const Seo = ({ description, title }) => {
+  const queryData = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `)
+
+  const metaTitle = title
+    ? `${title} | ${queryData.site?.siteMetadata?.title}`
+    : queryData.site?.siteMetadata?.title
+  const metaDescription =
+    description || queryData.site?.siteMetadata?.description
+
+  return (
+    <Helmet>
+      <title>{metaTitle}</title>
+      <meta name="description" content={metaDescription} />
+    </Helmet>
+  )
+}
+
+export default Seo
